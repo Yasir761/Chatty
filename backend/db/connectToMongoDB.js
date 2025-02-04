930456341710ef76776ec3a84605753172ecc0d1
Yasir761 <myasiransari16@gmail.com>
2025-02-04 18:36:38 +0530

    Make more improvements

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -1,9 +1,15 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+console.log("Database Name:  " + process.env.MONGODB_URL);
 
 const connectToMongoDB = async() => {
     try{
         await mongoose.connect(process.env.MONGODB_URL);
         console.log("Connected to MongoDB");
+        
     }
     catch(error){
        console.log("Error while connecting to MongoDB", error.message);
